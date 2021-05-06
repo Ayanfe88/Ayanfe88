665e6e5cc273846b018350aef4078bb33e564028
Ayanfe88 <peleyeju_femi@yahoo.com>
2021-05-06 17:33:45 +0300

    Home Slider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = []; // all components is routed here
+const routes: Routes = [
+  { path: "HomeComponent", component: HomeComponent },
+  { path: "Home", redirectTo: "homecomponent" }
+]; // all components is routed here
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
